4c00433ed0c1fb0d3a57eb7b7985be8f6a845fce
Kevin Zukker <kevin_zukker@icloud.com>
2024-06-16 11:54:16 +0300

    a

--- resources/js/Pages/GMaps.vue
+++ resources/js/Pages/GMaps.vue
@@ -15,45 +15,42 @@ import Radar from '@/Pages/Radar.vue';
     <AuthenticatedLayout>
         
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <!-- <div id="map" class="h-96 rounded-md border-2 border-grat-200"></div> -->
-                <!-- <Radar ref="radar" @location-selected="updateLocation" @markerPoints="setMarkerPoints" /> -->
-                <Radar ref="radar" @location-selected="updateLocation" />
+            <div class="sm:px-6 lg:px-8 flex flex-wrap">
+                <Radar ref="radar" class="w-1/2" @location-selected="updateLocation" />
 
-                <div v-if="editMode == false" class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-2 mt-2">
+                <div v-if="editMode == false" class="bg-white overflow-hidden sm:rounded-sm pl-4 w-1/2 flex items-center">
                     <form @submit.prevent="addPoint" class="flex flex-col gap-2">
                         <input v-model="name" type="text" step="any" placeholder="Name"
-                            class="rounded border-2 border-gray-200">
-                        <textarea v-model="description" name="description" id="description" cols="30" rows="4"
-                            placeholder="Description" class="rounded border-2 border-gray-200 resize-none"></textarea>
+                            class="rounded border-2 border-black">
+                        <textarea v-model="description" name="description" id="description" cols="40" rows="6"
+                            placeholder="Description" class="rounded border-2 border-black resize-none"></textarea>
                         <input v-model="newPoint.lat" type="number" step="any" placeholder="Latitude"
-                            class="rounded border-2 border-gray-200">
+                            class="rounded border-2 border-black">
                         <input v-model="newPoint.lng" type="number" step="any" placeholder="Longitude"
-                            class="rounded border-2 border-gray-200">
-                        <button type="submit" class="rounded border-2 border-gray-200 px-4 py-2 ">Add Point</button>
+                            class="rounded border-2 border-black">
+                        <button type="submit" class="rounded border-2 border-black px-4 py-2 ">Add Point</button>
                     </form>
                 </div>
-                <div v-else class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-2 mt-2">
-                    <p class="py-2 text-gray-400 font-bold">EDIT MODE ENABLED</p>
+                <div v-else class="bg-white overflow-hidden sm:rounded-sm pl-4 w-1/2 flex items-center">
                     <form @submit.prevent="updateMarker" class="flex flex-col gap-2">
                         <input v-model="name" type="text" step="any" placeholder="Name"
-                            class="rounded border-2 border-gray-200">
-                        <textarea v-model="description" name="description" id="description" cols="30" rows="4"
-                            placeholder="Description" class="rounded border-2 border-gray-200 resize-none"></textarea>
+                            class="rounded border-2 border-gray-400">
+                        <textarea v-model="description" name="description" id="description" cols="40" rows="6"
+                            placeholder="Description" class="rounded border-2 border-gray-400 resize-none"></textarea>
                         <input v-model="newPoint.lat" type="number" step="any" placeholder="Latitude"
-                            class="rounded border-2 border-gray-200">
+                            class="rounded border-2 border-gray-400">
                         <input v-model="newPoint.lng" type="number" step="any" placeholder="Longitude"
-                            class="rounded border-2 border-gray-200">
-                        <button type="submit" class="rounded border-2 border-gray-200 px-4 py-2 ">Edit marker</button>
+                            class="rounded border-2 border-gray-400">
+                        <button type="submit" class="rounded border-2 border-gray-400 px-4 py-2 ">Update</button>
                     </form>
                 </div>
                 <div v-for="marker in mapPoints" :key="marker.id"
                     class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-2 mt-2 flex">
                     <div class="w-full flex flex-col justify-center ">
-                        <h2 class="text-lg font-bold">{{ marker.name }}</h2>
-                        <p v-if="marker.description" class="text-sm">{{ marker.description }}</p>
+                        <h2 class="text-xl font-bold">{{ marker.name }}</h2>
+                        <p v-if="marker.description" class="text-lg">{{ marker.description }}</p>
                     </div>
-                    <div class="w-fit flex gap-2">
+                    <div class="flex flex-row p-4 gap-2">
                         <button @click="focusOnMarker(marker.lat, marker.lng)"
                             class="w-fit p-2 border-2 border-gray-200 rounded" type="submit">Focus</button>
                         <button @click="editMarker(marker)" class="w-fit px-4 py-2 border-2 border-gray-200 rounded"
@@ -105,19 +102,12 @@ export default {
             this.$refs.radar.focusOnPoint(lat, lng);
         },
 
-        // setMarkerPoints(points) {
-        //     this.mapPoints = [];
-        //     this.mapPoints = points;
-        //     console.log(points);
-        // },
-
         updateLocation(location) {
             this.newPoint.lat = location.lat;
             this.newPoint.lng = location.lng;
         },
 
         async addPoint() {
-            // Make a POST request to your API to save the new point's coordinates
             await axios.post('/api/google-maps/markers', {
                 name: this.name,
                 description: this.description,
@@ -125,12 +115,10 @@ export default {
                 lng: this.newPoint.lng,
             });
 
-            // Clear the form
             this.newPoint = { lat: null, lng: null };
             this.name = '';
             this.description = '';
 
-            // Load the markers
             this.$refs.radar.loadMarkers();
 
             this.fetchMarkers();
@@ -146,7 +134,6 @@ export default {
         },
 
         updateMarker() {
-            // Make a PUT request to your API to update the point with the specified latitude and longitude
             axios.put(`/api/google-maps/markers/${this.editMarkerId}`, {
                 name: this.name,
                 description: this.description,
@@ -158,13 +145,11 @@ export default {
                 },
             })
                 .then(() => {
-                    // Clear the form
                     this.newPoint = { lat: null, lng: null };
                     this.name = '';
                     this.description = '';
                     this.editMode = false;
 
-                    // Load the markers
                     this.$refs.radar.clearMarkers();
                     this.$refs.radar.loadMarkers();
 
@@ -173,15 +158,11 @@ export default {
                 .catch((error) => {
                     console.error(error);
                 });
-
-
         },
 
         async deletePoint(id) {
-            // Make a DELETE request to your API to delete the point with the specified latitude and longitude
             await axios.delete(`/api/google-maps/markers/${id}`);
 
-            // Remove the point from the mapPoints array
             this.$refs.radar.clearMarkers();
             this.$refs.radar.loadMarkers();
 
@@ -189,8 +170,6 @@ export default {
         },
     },
     mounted() {
-        // Executes the fetchData method when the component is mounted.
-        // this.fetchData();
         this.fetchMarkers();
     },
 };
